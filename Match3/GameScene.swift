a67ef81092b9be8b065e586b09be988bf0294be3
ricky <ricky@glass.photo>
2021-07-11 13:30:11 -0700

    first commit

---
+++ Match3/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Match3
+//
+//  Created by ricky cancro on 7/11/21.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
---
+++ Match3/GameViewController.swift
@@ -0,0 +1,45 @@
+//
+//  GameViewController.swift
+//  Match3
+//
+//  Created by ricky cancro on 7/11/21.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func loadView() {
+        view = SKView(frame: .zero)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
